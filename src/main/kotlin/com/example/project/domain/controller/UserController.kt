8618732563747f017a02155506fc5d4a6689ca45
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-05-14 22:40:27 +0900

    feat: コントローラのurl追加

--- src/main/kotlin/com/example/project/domain/controller/UserController.kt
+++ src/main/kotlin/com/example/project/domain/controller/UserController.kt
@@ -3,15 +3,19 @@ package com.example.project.domain.controller
 import com.example.project.domain.form.UserForm
 import com.example.project.domain.service.UserService
 import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("api/v1/user")
 @RequiredArgsConstructor
 class UserController(private val userService: UserService) {
   /**
    * ユーザー新規登録API
    */
+  @GetMapping("/signup")
   fun userSignup(@RequestBody userForm: UserForm): String {
     return userService.userSignUp(userForm)
   }
