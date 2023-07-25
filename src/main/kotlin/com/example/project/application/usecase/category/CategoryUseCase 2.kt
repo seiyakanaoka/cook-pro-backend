37e8514d4e6efe819c0f8d86d784bb63e9dcb945
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-26 08:37:25 +0900

    fix: 不要に作成されたファイルを削除

--- src/main/kotlin/com/example/project/application/usecase/category/CategoryUseCase 2.kt
+++
@@ -1,7 +0,0 @@
-package com.example.project.application.usecase.category
-
-import com.example.project.application.dto.category.CategoriesDto
-
-interface CategoryUseCase {
-  fun generateCategories(): CategoriesDto
-}
