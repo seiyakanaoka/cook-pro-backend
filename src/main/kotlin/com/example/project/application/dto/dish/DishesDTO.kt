9f3efa114d1fc6392bdcda75840ddbb3bd3b5286
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-27 10:40:48 +0900

    feat: 詳細用のDTOを作成

--- src/main/kotlin/com/example/project/application/dto/dish/DishesDTO.kt
+++ src/main/kotlin/com/example/project/application/dto/dish/DishesDTO.kt
@@ -3,9 +3,9 @@ package com.example.project.application.dto.dish
 import lombok.Data
 
 @Data
-data class DishDTO(
+data class DishesDTO(
   val dishId: String,
   val dishName: String,
-  val image: List<DishImageDTO>,
+  val image: DishImageDTO,
   val dishCreateRequiredTime: Int
 )
