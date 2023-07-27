58f3f62bd7e0375a0cdbc6453b1eb9fe93fc6afa
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-27 15:54:31 +0900

    feat: componentにする

--- src/main/kotlin/com/example/project/presentation/mapper/dish/DishResponseMapper.kt
+++ src/main/kotlin/com/example/project/presentation/mapper/dish/DishResponseMapper.kt
@@ -2,7 +2,9 @@ package com.example.project.presentation.mapper.dish
 
 import com.example.project.application.dto.dish.DishSearchDTO
 import com.example.project.presentation.response.dish.DishSearchResponse
+import org.springframework.stereotype.Component
 
+@Component
 class DishResponseMapper {
   fun toSearchResponse(dishesSearch: List<DishSearchDTO>): List<DishSearchResponse> {
     return dishesSearch.map { it -> DishSearchResponse(it.dishId, it.dishName) }
