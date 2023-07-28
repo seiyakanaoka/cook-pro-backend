598ea3425a2f0d4298bc3094e870f709c8108789
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-28 13:28:00 +0900

    feat: スキーマと実装を修正

--- src/main/kotlin/com/example/project/infrastructure/persistence/dish/JpaDishRepository.kt
+++ src/main/kotlin/com/example/project/infrastructure/persistence/dish/JpaDishRepository.kt
@@ -31,5 +31,8 @@ interface JpaDishRepository : DishRepository, JpaRepository<Dish, String> {
   @Query("select di from Dish d inner join DishImage di on d.dishId = di.dish.dishId where d.dishId = :dishId")
   override fun findByDishImages(@Param("dishId") dishId: String): List<DishImage>
 
-  override fun findByCategoriesCategoryIdInOrderByCreateTimestampDesc(categoryIds: List<String>): List<Dish>
+  override fun findByCategoryDishes(
+    @Param("userId") userId: String,
+    @Param("categoryIds") categoryIds: List<String>
+  ): List<Dish>
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,11 +16,13 @@ create table if not exists user (
 create table if not exists dish (
   dish_id varchar(36) not null primary key comment '料理ID',
   user_id varchar(36) not null comment 'ユーザーID',
+  dish_category_id varchar(36) not null comment '料理とカテゴリーの中間ID',
   dish_name varchar(255) not null comment '料理名',
   dish_create_required_time bigint(20) unsigned not null comment '所要時間',
   create_timestamp timestamp default current_timestamp comment '作成日時',
   update_timestamp timestamp default current_timestamp on update current_timestamp comment '更新日時',
-  foreign key (user_id) references user (user_id) on delete cascade
+  foreign key (user_id) references user (user_id) on delete cascade,
+  foreign key (dish_category_id) references user (dish_category_id) on delete cascade
 );
 
 
