9c460596eb33a85fc3c49006fef4f5d62cdaa025
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-01 14:27:13 +0900

    feat: usecaseの実装を修正

--- src/main/kotlin/com/example/project/application/usecase/user/UserUseCase.kt
+++ src/main/kotlin/com/example/project/application/usecase/user/UserUseCase.kt
@@ -4,11 +4,9 @@ import com.amazonaws.services.s3.AmazonS3
 import com.example.project.application.dto.user.UserDTO
 import com.example.project.application.dto.user.UserFormDTO
 import com.example.project.application.dto.user.UserPatchFormDTO
-import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient
 
 interface UserUseCase {
   val s3: AmazonS3
-  val cognito: CognitoIdentityProviderClient
 
   fun createUser(userFormDTO: UserFormDTO): Unit
 
--- src/main/kotlin/com/example/project/application/usecase/user/UserUseCaseImpl.kt
+++ src/main/kotlin/com/example/project/application/usecase/user/UserUseCaseImpl.kt
@@ -2,33 +2,25 @@ package com.example.project.application.usecase.user
 
 import com.example.project.application.dto.user.UserDTO
 import com.example.project.application.dto.user.UserFormDTO
+import com.example.project.application.dto.user.UserPatchFormDTO
 import com.example.project.application.mapper.user.UserMapper
-import com.example.project.config.aws.CognitoConfig
 import com.example.project.config.aws.S3Config
 import com.example.project.domain.repository.user.UserRepository
-import com.example.project.presentation.form.user.UserPatchForm
 import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminUpdateUserAttributesRequest
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType
 
 @Service
 @RequiredArgsConstructor
 class UserUseCaseImpl(
   private val userRepository: UserRepository,
   private val s3Client: S3Config,
-  private val cognitoConfig: CognitoConfig,
   private val userMapper: UserMapper
 ) : UserUseCase {
   override val s3 = s3Client.s3Client()
-  override val cognito = cognitoConfig.cognitoClient()
 
   @Value("\${aws.s3.bucket.name}")
   private val bucketName = ""
-
-  @Value("\${aws.cognito.user-pool-id}")
-  private val userPoolId = ""
 
   /**
    * 新規登録
@@ -52,22 +44,12 @@ class UserUseCaseImpl(
    * ユーザー名編集
    * TODO: CognitoもしくはDBへの変更が失敗した場合、両方とも変更しない構造にする
    */
-  override fun patchUserName(userId: String, email: String, userNameForm: UserPatchForm) {
-    val user = userRepository.findById(userId).orElseThrow() { RuntimeException() }
-    user.userName = userNameForm.userName
+  override fun patchUserName(userId: String, userPatchFormDTO: UserPatchFormDTO) {
+    val user = userRepository.findById(userId).orElseThrow() { RuntimeException("ユーザーが存在しません") }
+    user.displayUserName = userPatchFormDTO.displayUserName
+    user.email = userPatchFormDTO.email
+    user.telNumber = userPatchFormDTO.telNumber
     userRepository.save(user)
-    val request: AdminUpdateUserAttributesRequest = AdminUpdateUserAttributesRequest.builder()
-      .userPoolId(userPoolId)
-      .username(email)
-      .userAttributes(
-        AttributeType.builder()
-          .name("preferred_username")
-          .value(userNameForm.userName)
-          .build()
-      )
-      .build()
-
-    cognito.adminUpdateUserAttributes(request)
   }
 
   /**
