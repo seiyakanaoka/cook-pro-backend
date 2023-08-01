e20a0042f7152dcab08c02300209dcdd244ede16
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-01 14:53:15 +0900

    feat: s3を呼ぶ準備をする

--- src/main/kotlin/com/example/project/application/usecase/user/UserUseCase.kt
+++ src/main/kotlin/com/example/project/application/usecase/user/UserUseCase.kt
@@ -1,18 +1,14 @@
 package com.example.project.application.usecase.user
 
-import com.amazonaws.services.s3.AmazonS3
 import com.example.project.application.dto.user.UserDTO
 import com.example.project.application.dto.user.UserFormDTO
 import com.example.project.application.dto.user.UserPatchFormDTO
 
 interface UserUseCase {
-  val s3: AmazonS3
 
   fun createUser(userFormDTO: UserFormDTO): Unit
 
   fun getUser(userId: String): UserDTO
 
-  fun patchUserName(userId: String, userPatchFormDTO: UserPatchFormDTO): Unit
-
-  fun getImageURL(objectKey: String): String
+  fun patchUser(userId: String, userPatchFormDTO: UserPatchFormDTO): Unit
 }
--- src/main/kotlin/com/example/project/application/usecase/user/UserUseCaseImpl.kt
+++ src/main/kotlin/com/example/project/application/usecase/user/UserUseCaseImpl.kt
@@ -4,23 +4,25 @@ import com.example.project.application.dto.user.UserDTO
 import com.example.project.application.dto.user.UserFormDTO
 import com.example.project.application.dto.user.UserPatchFormDTO
 import com.example.project.application.mapper.user.UserMapper
-import com.example.project.config.aws.S3Config
 import com.example.project.domain.repository.user.UserRepository
+import com.example.project.util.aws.S3
 import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 @RequiredArgsConstructor
 class UserUseCaseImpl(
   private val userRepository: UserRepository,
-  private val s3Client: S3Config,
+  private val s3: S3,
   private val userMapper: UserMapper
 ) : UserUseCase {
-  override val s3 = s3Client.s3Client()
-
   @Value("\${aws.s3.bucket.name}")
   private val bucketName = ""
+
+  @Value("\${aws.s3.bucket.name.object-key-prefix}")
+  private val userObjectKeyPrefix = ""
 
   /**
    * 新規登録
@@ -37,27 +39,22 @@ class UserUseCaseImpl(
    */
   override fun getUser(userId: String): UserDTO {
     val user = userRepository.findById(userId).orElseThrow { RuntimeException() }
-    return userMapper.toDto(user, user.userImageKey?.let { getImageURL(it) })
+    return userMapper.toDto(user, user.userImageKey?.let {
+      s3.getImageURL(it)
+    })
   }
 
   /**
    * ユーザー名編集
    * TODO: CognitoもしくはDBへの変更が失敗した場合、両方とも変更しない構造にする
    */
-  override fun patchUserName(userId: String, userPatchFormDTO: UserPatchFormDTO) {
+  override fun patchUser(userId: String, userPatchFormDTO: UserPatchFormDTO) {
     val user = userRepository.findById(userId).orElseThrow() { RuntimeException("ユーザーが存在しません") }
     user.displayUserName = userPatchFormDTO.displayUserName
     user.email = userPatchFormDTO.email
     user.telNumber = userPatchFormDTO.telNumber
+    val objectKey = userObjectKeyPrefix + UUID.randomUUID().toString()
+//    s3.uploadImage(objectKey, )
     userRepository.save(user)
-  }
-
-  /**
-   * S3の指定したバケットの画像urlを取得する
-   * @param objectKey オブジェクトキー名
-   */
-  override fun getImageURL(objectKey: String): String {
-    val url = s3.getUrl(bucketName, objectKey) ?: throw IllegalStateException("URL is null")
-    return url.toString()
   }
 }
