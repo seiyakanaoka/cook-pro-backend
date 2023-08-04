13dc519900c638e8c42736817f12db9332db1e3c
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-04 22:43:40 +0900

    feat: controllerを作成

--- src/main/kotlin/com/example/project/presentation/controller/dish/DishControllerImpl.kt
+++ src/main/kotlin/com/example/project/presentation/controller/dish/DishControllerImpl.kt
@@ -47,6 +47,13 @@ class DishControllerImpl(
     dishResponseMapper.toDetailResponse(dishUseCaseImpl.getDish(userId, dishId))
 
   /**
+   * 料理削除API
+   */
+  @DeleteMapping("/dish/{dishId}")
+  override fun deleteDish(@PathVariable dishId: String): Unit {
+  }
+
+  /**
    * 料理検索一覧取得API
    */
   @GetMapping("/dish/search")
