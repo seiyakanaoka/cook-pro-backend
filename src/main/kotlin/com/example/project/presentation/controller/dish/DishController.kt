1576c5a1134a1fd9e154841630e4896163506543
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-04 17:17:19 +0900

    feat: controllerのinterfaceに型を作成

--- src/main/kotlin/com/example/project/presentation/controller/dish/DishController.kt
+++ src/main/kotlin/com/example/project/presentation/controller/dish/DishController.kt
@@ -2,12 +2,14 @@ package com.example.project.presentation.controller.dish
 
 import com.example.project.application.dto.dish.DishProcessesDTO
 import com.example.project.domain.enums.category.CategoryEnum
+import com.example.project.presentation.form.dish.DishForm
 import com.example.project.presentation.response.dish.DishDetailResponse
 import com.example.project.presentation.response.dish.DishMaterialsResponse
 import com.example.project.presentation.response.dish.DishesResponse
 import com.example.project.presentation.response.dish.DishesSearchResponse
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestAttribute
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 
 interface DishController {
@@ -17,6 +19,8 @@ interface DishController {
   ): DishesResponse?
 
   fun getDish(@RequestAttribute("userId") userId: String, @PathVariable dishId: String): DishDetailResponse
+
+  fun postDish(@RequestAttribute("userId") userId: String, @RequestBody dishForm: DishForm): DishDetailResponse
 
   fun getSearchDishes(
     @RequestAttribute("userId") userId: String,
