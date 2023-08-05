86f239d54f35d78cf74df982c51f8f85d9489ccc
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-05 22:16:07 +0900

    feat: マッパーを作成

--- src/main/kotlin/com/example/project/application/mapper/dish/DishMapper.kt
+++ src/main/kotlin/com/example/project/application/mapper/dish/DishMapper.kt
@@ -1,6 +1,7 @@
 package com.example.project.application.mapper.dish
 
 import com.example.project.application.dto.dish.DishFormDTO
+import com.example.project.application.dto.dish.PutDishFormDTO
 import com.example.project.domain.model.category.Category
 import com.example.project.domain.model.dish.Dish
 import com.example.project.domain.model.material.Material
@@ -17,6 +18,18 @@ class DishMapper {
     dish.categories = categories
     val materials =
       dishFormDTO.materials.map { it -> Material(it.materialId, it.materialName, it.quantity, it.unit, dish) }
+        .toMutableList()
+    dish.materials = materials
+    return dish
+  }
+
+  fun toPutDishDomainEntity(user: User, putDishFormDTO: PutDishFormDTO): Dish {
+    val dishId = UUID.randomUUID().toString()
+    val dish = Dish(dishId, user, putDishFormDTO.dishName, putDishFormDTO.createRequiredTime)
+    val categories = putDishFormDTO.category.map { it -> Category(it.categoryId, it.categoryType) }.toMutableList()
+    dish.categories = categories
+    val materials =
+      putDishFormDTO.materials.map { it -> Material(it.materialId, it.materialName, it.quantity, it.unit, dish) }
         .toMutableList()
     dish.materials = materials
     return dish
